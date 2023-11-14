d9267661ccf0cd98f09e69d3999587a4387b793f
stugceates <stugceates@gmail.com>
2023-11-14 15:36:49 +0000

    add search to page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <h1>Dictionary Project</h1>
+      <div className="container">
+        <h1>Dictionary Project</h1>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This project coded by Selin Ates and open sourced on{" "}
+          <a href="https://github.com/stugceates/dictionary-project">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
