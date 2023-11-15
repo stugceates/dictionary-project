dc0c52d55f050a7dc755ec8790f01370912198d4
stugceates <stugceates@gmail.com>
2023-11-15 11:12:11 +0000

    add dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(" ");
 
+  function handleResponse(response) {
+    console.log(response.data.meanings[0]);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}`);
+
+    //documentation:https://www.shecodes.io/learn/apis/dictionary
+
+    let apiKey = "fa2f0ab0044e0f6ed0fo3e30511f6tbc";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
