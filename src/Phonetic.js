46742c32e5f072ac5bd5689cbc38fe5396abfd4a
stugceates <stugceates@gmail.com>
2023-11-16 16:10:44 +0000

    add word's phonetic to the page

---
+++ src/Phonetic.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Phonetic(props) {
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <h2>{props.phonetic}</h2>
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Results.css";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="results">
-        <h4>{props.results.word}</h4>
+        <section>
+          <h4>{props.results.word}</h4>
+          <Phonetic phonetic={props.results.phonetic} />
+        </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <section key={index}>
